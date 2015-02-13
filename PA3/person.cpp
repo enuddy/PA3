0a4c8365490e42279134f829f5063aa699052c58
NFosterJava <whycantifindausername12@gmail.com>
2015-02-13 11:51:35 -0500

    Update person.cpp
    Defined some functions

--- PA3/person.cpp
+++ PA3/person.cpp
@@ -3,9 +3,66 @@
 
 Person::Person()
 {
+  mId = "";
+  mName = "";
+  mPhoneNumber = "";
 }
 
+Person::Person(string id, string name, string phoneNumber)
+{
+  mId = id;
+  mName = name;
+  mPhoneNumber = phoneNumber;
+}
 
 Person::~Person()
 {
+}
+
+Person::string getId()
+{
+  return mId;
+}
+
+Person::string getName()
+{
+  return mName;
+}
+
+Person::string getPhoneNumber()
+{
+  return mPhoneNumber;
+}
+
+Person::void setID(string newId)
+{
+  mId = newId;
+}
+
+Person::void setname(string newName)
+{
+  mName = newName;
+}
+
+Person::void setPhoneNumber(string newPhone)
+{
+  mPhoneNumber = newPhone;
+}
+
+Person::bool operator==(string id)
+{
+  return mId == id;
+}
+
+Person::bool operator==(Person rhs)
+{
+  return mId == rhs.mId;
+}
+
+}
+}
+}
+}
+}
+}
 }
